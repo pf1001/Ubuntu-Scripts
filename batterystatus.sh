eb9f8379b62d3e067a1a1de33d5e988597bce083
pf1001 <32334859+pf1001@users.noreply.github.com>
2021-07-17 16:38:23 +0100

    Update batterystatus.sh
    updated to notify at 10%

--- batterystatus.sh
+++ batterystatus.sh
@@ -7,7 +7,7 @@ do
 	export DISPLAY=:0.0
 	batterylevel=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage" | awk '{print $2}' | grep -P -o '[0-9]+(?=%)'`
 
-	if [ $batterylevel -le 70 ]; then
+	if [ $batterylevel -le 10 ]; then
 		notify-send --urgency=CRITICAL "Battery Low" "Level: ${batterylevel}%"
 		paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
 		exit 0;
