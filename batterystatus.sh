95397c5ae60abac7bb6e345dd163077358a7cf0a
pf1001 <32334859+pf1001@users.noreply.github.com>
2021-07-17 16:29:29 +0100

    Create batterystatus.sh

---
+++ batterystatus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Battery status check on Ubuntu 20.04
+# Set the script to run as a cron job. e.g to run every 5 minutes.
+
+while true
+do
+	export DISPLAY=:0.0
+	batterylevel=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage" | awk '{print $2}' | grep -P -o '[0-9]+(?=%)'`
+
+	if [ $batterylevel -le 70 ]; then
+		notify-send --urgency=CRITICAL "Battery Low" "Level: ${batterylevel}%"
+		paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+		exit 0;
+	else
+		exit 0;
+	fi
+
+done
